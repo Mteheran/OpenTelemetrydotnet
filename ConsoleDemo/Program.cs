5c25a6aebe04b95e23e3ad4c66781c666e7b9681
Miguel Teheran <miguelteheran@gmail.com>
2022-10-06 18:18:28 -0500

    basic demo in console

---
+++ ConsoleDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Logs;
+
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.AddOpenTelemetry(options =>
+            {
+                options.AddConsoleExporter();
+            });
+});
+
+var logger = loggerFactory.CreateLogger<Program>();
+logger.LogInformation("Hello community");
